bb890fdd432b3925a96dccfe4c8c389dbf66e94c
Valdesoiro <valdesoiro@Galaxy-S6-edge-Irvin.localdomain>
2019-10-17 17:22:22 +0200

    add getListItem and render it

--- play-with-react/src/App.js
+++ play-with-react/src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [data, setData] = useState([])
+
+  const getListData = () => {
+      fetch('https://pokeapi.co/api/v2/pokemon?offset=1&limit=999')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(myJson) {
+      //console.log(myJson);
+      setData(myJson.results);
+    });
+  }
+getListData()
+
+const listItems = data.map((element, key) =>
+  <li key={key} >{element.name}</li>
+);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {listItems}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
